c063d99506394fe875a64a913dc5d1515d841ebd
PavithraSai849 <lmsplatform46@gmail.com>
2024-11-06 00:58:50 -0500

    host env variable change

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://e-learn-platform.vercel.app/" ||  "http://localhost:5000/",
 });
 
 axiosInstance.interceptors.request.use(
